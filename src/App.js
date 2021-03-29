80055416b3ad77037df3b06553b752b6843669c8
agupta <amol.gupta@us.sios.com>
2021-03-29 13:09:06 -0400

    [B-001] Initial setup portfolio website

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Routes from "./Routes";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+            <div>
+              <Routes />
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SidebarItems.js
@@ -0,0 +1,19 @@
+const SidebarItems = [
+    {
+        id: 0,
+        name: "Dashboard",
+        path: '/',
+    },
+    {
+        id: 1,
+        name: "Intro",
+        path: '/intro',
+    },
+    {
+        id: 2,
+        name: "Education",
+        path: '/education',
+    },
+];
+
+export default SidebarItems;
